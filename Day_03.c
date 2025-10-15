f5ef76807834bf73640d3640a3eab536e07dc9b0
Kavya Gupta <kavygupta0605@gmail.com>
2025-10-15 14:32:28 +0530

    Add files via upload

---
+++ Day_03.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Enter value of K: ");
+    scanf("%d", &k);
+
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    
+    printf("kth Minimum element = %d\n", arr[k - 1]);
+    printf("kth Maximum element = %d\n", arr[n - k]);
+    
+    return 0;
+}
